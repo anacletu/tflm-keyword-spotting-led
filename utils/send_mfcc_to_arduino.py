06a3a57d867b2b571ed253f37330cfc9e6448e31
Eric <eric.anacleto@gmail.com>
2025-06-17 15:45:23 +0200

    Updating model parameters

--- utils/send_mfcc_to_arduino.py
+++ utils/send_mfcc_to_arduino.py
@@ -7,12 +7,12 @@ import sounddevice as sd # For recording
 import librosa           # For MFCC calculation
 
 # --- Configuration Parameters ---
-SERIAL_PORT = '/dev/cu.usbmodem2201'  # Replace if Arduino's serial port differs
+SERIAL_PORT = '/dev/cu.usbmodem11101'  # Replace if Arduino's serial port differs
 BAUD_RATE = 115200
 
 # Model Input Quantization Parameters (from TFLite input_details)
-MODEL_INPUT_SCALE = 3.016157627105713 
-MODEL_INPUT_ZERO_POINT = 63
+MODEL_INPUT_SCALE = 2.7671010494232178 
+MODEL_INPUT_ZERO_POINT = 64
 
 # Model Input Dimensions & Audio Parameters
 N_MFCC = 13
